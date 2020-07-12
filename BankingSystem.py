4f5077701679824e790d72f852ff0af5ed07a190
Tim Demetriades <tim.demetriades@gmail.com>
2020-07-11 21:58:59 -0400

    BankingSystem - Stage 3/4

--- BankingSystem.py
+++ BankingSystem.py
@@ -3,37 +3,50 @@
 # Date: 06/30/2020
 
 from random import randint
+import sqlite3   
 import sys
 
-def randint_with_n_digits(n):
-    range_start = 10 ** (n - 1)
-    range_end = (10 ** n) - 1
-    return randint(range_start, range_end)
 
-class Banking_System:
-    accounts = {}       # dict of accounts created
+# define connection and cursor
+connection = sqlite3.connect('card.s3db')   # cards database
+cursor = connection.cursor()
+
+# create table query
+create_card_table = '''CREATE TABLE IF NOT EXISTS 
+    card (
+    id INTEGER,
+    number TEXT,
+    pin TEXT,
+    balance INTEGER DEFAULT 0
+    );'''
+
+# execute query
+cursor.execute(create_card_table)
+
+# commit changes
+connection.commit()
+
+
+class BankingSystem:
     current_user = ""   # holds account_identifier after user logs in    
     BIN = 400000        # Bank Identification Number
 
     def create_account(self):
-        user_data = {}  # dict to insert into accounts
-
         self.account_identifier = str(randint_with_n_digits(9))      
-        card_number = str(self.BIN) + self.account_identifier
-        card_number = card_number + self.calc_checksum(card_number)
-        card_PIN =  str(randint_with_n_digits(4))
+        self.card_number = str(self.BIN) + self.account_identifier
+        self.card_number = self.card_number + self.calc_checksum(self.card_number)
+        self.card_PIN =  str(randint_with_n_digits(4))
+        self.balance = 0
 
-        user_data[self.account_identifier] = {
-            'Card Number': card_number,
-            'PIN': card_PIN,
-            'Balance': 0
-        }
-
-        Banking_System.accounts.update(user_data)       # add new account to accounts dict
+        self.insert_into_card_table(self.account_identifier, self.card_number, self.card_PIN, self.balance)     # insert into db
 
         print('\nYour card has been created')
-        print(f'Your card number:\n{card_number}')
-        print(f'Your card PIN:\n{card_PIN}\n')
+        print(f'Your card number:\n{self.card_number}')
+        print(f'Your card PIN:\n{self.card_PIN}\n')
+
+    def insert_into_card_table(self, id, number, pin, balance):
+        cursor.execute('INSERT INTO card VALUES (?, ?, ?, ?)',(id, number, pin, balance))   # need to use this format with ?
+        connection.commit()
 
     def calc_checksum(self, card_number):
         # using luhn algorithm
@@ -52,19 +65,24 @@ class Banking_System:
             return str(0)                                       # checksum is 0
         else:
             return str(10 - sum % 10)                           # checksum is remainder to next number divisible by 10
+
     def log_into_account(self):
-        if Banking_System.accounts == {}:       # if no accounts have been made yet
+        cursor.execute('SELECT COUNT(id) FROM card')
+        num_of_cards = cursor.fetchall()
+        if num_of_cards == [(0,)]:
             print('\nYou must create an account first.\n')
         else:
             entered_card = input('\nEnter your card number:\n')
             entered_PIN = input('Enter your PIN:\n')
 
-            account = entered_card[6:15]    # this should match the account_identifier of a card number (digits 6 to 14) 
+            account = entered_card[6:15]    # this should match the account_identifier of a card number (digits 6 to 14) and also the id column
 
-            if account in Banking_System.accounts:     # if account exists
-                if Banking_System.accounts[account]['Card Number'] == entered_card and Banking_System.accounts[account]['PIN'] == entered_PIN:      # and matches correct number and pin
+            cursor.execute('SELECT number, pin FROM card WHERE id = ?', (account,))
+            records = cursor.fetchall()
+            if records != []:
+                if records[0][0] == entered_card and records[0][1] == entered_PIN:
                     print('\nYou have successfully logged in!\n')
-                    Banking_System.current_user = account
+                    BankingSystem.current_user = account
                     self.account()
                 else:
                     print('\nWrong card number or PIN!\n')
@@ -79,9 +97,11 @@ class Banking_System:
             choice = input()
 
             if choice == '1':
-                print(f"\nBalance: {Banking_System.accounts[Banking_System.current_user]['Balance']}\n")
+                cursor.execute('SELECT balance FROM card WHERE id = ?', BankingSystem.current_user)
+                balance = cursor.fetchall()
+                print(f"\nBalance: {balance}\n")
             elif choice == '2':
-                Banking_System.current_user = ""
+                BankingSystem.current_user = ""
                 print('\nYou have successfully logged out!\n')
                 break
             elif choice == '0':
@@ -90,7 +110,13 @@ class Banking_System:
             else:
                 print('\nPlease enter either 1, 2, or 0 to exit.\n')
 
-banking_system = Banking_System()       # Instance of Banking_System
+
+def randint_with_n_digits(n):
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
+    return randint(range_start, range_end)
+
+banking_system = BankingSystem()       # Instance of BankingSystem
 
 # Main Loop
 while True:
